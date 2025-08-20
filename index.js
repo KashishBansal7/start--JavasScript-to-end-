a7f8ce5307ca38073dc91f1ea14a7ed5df5633ba
KashishBansal7 <kashishbansal002@gmail.com>
2025-08-20 23:16:52 +0530

    js practice set 1

---
+++ index.js
@@ -0,0 +1,27 @@
+// practice set 1st
+// question 1
+let say ="hello";
+let result = say+10;
+console.log(result); 
+
+// question 2
+console.log(typeof say);
+console.log(typeof result);
+// question 3
+const obj={
+    name: "kashish",
+    age: 21
+};
+// question 4
+ 
+obj.city = "delhi";
+console.log(obj);
+// question 5
+const dictionary ={
+    hello: "greeting",
+    namee: "kashish",
+    javascript: "programming language",
+    student: "learner",
+    book: "collection of pages"
+};
+console.log(dictionary);
